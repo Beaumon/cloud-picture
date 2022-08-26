ed17b2dce9cef4c757d8a703981009ae0cef0bc7
beaumon <61904805+Beaumon@users.noreply.github.com>
2022-08-26 22:12:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'cloud-picture.workers.dev'
+      url.hostname = 'github.cloud-picture.workers.dev'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
